761ade69a61373d432b20698a81cd9acce1ee0bd
Ethem Sarnıçlı <ethemsarnicli@gmail.com>
2025-05-01 16:18:32 +0300

    Add files via upload

---
+++ kotlindersleri/odevler/Odev3/app/src/main/java/com/example/odev3/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.odev3
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.buttonGecis)
+        button.setOnClickListener {
+            val intent = Intent(this, second::class.java)
+            startActivity(intent)
+        }
+
+
+
+    }
+}
